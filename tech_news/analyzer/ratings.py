6c0f807bdc07f35ba7be20c442be658d242ea3b9
Flávio João Félix <flaviojoaofelix.dev@gmail.com>
2023-02-09 23:33:37 -0300

    :sparkles: feat: req 10 - Crie a função top_5_categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,16 @@
+from tech_news.database import db
+
 # Requisito 10
 def top_5_categories():
     """Seu código deve vir aqui"""
+    data = list(
+        db.news.aggregate(
+            [
+                {"$group": {"_id": "$category", "count": {"$sum": 1}}},
+                {"$sort": {"count": -1, "_id": 1}},
+                {"$limit": 5},
+            ]
+        )
+    )
+
+    return [item["_id"] for item in data]
