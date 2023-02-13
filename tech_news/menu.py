79274eb43cead99bdbc8f6e0f95063dbb54fabb5
Flávio João Félix <flaviojoaofelix.dev@gmail.com>
2023-02-13 18:35:56 -0300

    :recycle: refactor: req 12 e 13 - cleaning the code of solutions that did not pass the test

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -10,42 +10,7 @@ from tech_news.analyzer.ratings import top_5_categories
 
 # Requisitos 11 e 12
 
-# menu = {
-#     "options": {
-#         0: {
-#             "title": "Popular o banco com notícias;",
-#             "question": "Digite quantas notícias serão buscadas:",
-#             "function": get_tech_news,
-#         },
-#         1: {
-#             "title": "Buscar notícias por título;",
-#             "question": "Digite o título:",
-#             "function": search_by_title,
-#         },
-#         2: {
-#             "title": "Buscar notícias por data;",
-#             "question": "Digite a data no formato aaaa-mm-dd:",
-#             "function": search_by_date,
-#         },
-#         3: {
-#             "title": "Buscar notícias por categoria;",
-#             "question": "Digite a categoria:",
-#             "function": search_by_category,
-#         },
-#         4: {
-#             "title": "Listar top 5 categorias;",
-#             "question": False,
-#             "function": top_5_categories,
-#         },
-#         5: {
-#             "title": "Sair.",
-#             "question": "Encerrando script",
-#             "function": False,
-#         },
-#     },
-# }
-
-options = {
+menu_options = {
     0: {
         "title": "Popular o banco com notícias;",
         "question": "Digite quantas notícias serão buscadas:",
@@ -79,46 +44,9 @@ options = {
 }
 
 
-# def create_menu():
-#     menu_options = "Selecione uma das opções a seguir:\n"
-
-#     for option in menu["options"]:
-#         menu_options += f" {option} - {menu['options'][option]['title']}\n"
-
-#     return menu_options
-
-
-# def run_option(option):
-#     if menu["options"][option]["question"]:
-#         return menu["options"][option]["function"](
-#             input(menu["options"][option]["question"])
-#         )
-#     else:
-#         return menu["options"][option]["function"]
-
-
-# def analyzer_menu():
-#     menu_options_number = len(menu["options"]) - 1
-#     menu_options_range = range(0, menu_options_number)
-#     menu_options = create_menu()
-
-#     try:
-#         option = int(input(menu_options))
-#         if option in menu_options_range:
-#             result = run_option(option)
-#         elif option == menu_options_number:
-#             result = menu["options"][option]["question"]
-#         else:
-#             raise ValueError
-
-#         print(f"{result}", file=sys.stdout)
-#     except ValueError:
-#         print("Opção inválida", file=sys.stderr)
-
-
 class Menu:
     def __init__(self):
-        self.options = options
+        self.options = menu_options
 
     def get_title(self, option):
         return self.options[option]["title"]
@@ -129,13 +57,13 @@ class Menu:
     def get_function(self, option):
         return self.options[option]["function"]
 
-    def create_menu(self):
-        menu_options = "Selecione uma das opções a seguir:\n"
+    def create(self):
+        menu = "Selecione uma das opções a seguir:\n"
 
         for option in self.options:
-            menu_options += f" {option} - {self.options[option]['title']}\n"
+            menu += f" {option} - {self.options[option]['title']}\n"
 
-        return menu_options
+        return menu
 
     def check_choice(self, option):
         if option in self.options:
@@ -143,29 +71,10 @@ class Menu:
         return False
 
 
-# def analyzer_menu():
-#     try:
-#         menu = Menu()
-#         user_option = int(input(menu.create_menu()))
-
-#         if menu.get_question(user_option):
-#             run_option = menu.get_function(user_option)
-#             result = run_option(input(menu.get_question(user_option)))
-#         elif hasattr(menu.get_function(user_option), "__call__"):
-#             run_option = menu.get_function(user_option)
-#             result = run_option()
-#         else:
-#             result = menu.get_function(user_option)
-
-#         print(f"{result}", file=sys.stdout)
-#     except (ValueError, KeyError):
-#         print("Opção inválida", file=sys.stderr)
-
-
 def analyzer_menu():
     menu = Menu()
-    user_option = input(menu.create_menu())
-    result = ""
+    user_option = input(menu.create())
+    result = str()
 
     try:
         user_option = int(user_option)
